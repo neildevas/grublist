5ca4e1fbf5b00a14a373b67f3ea2a9c66da2cc27
Neil Devas <neil.devas97@gmail.com>
2021-05-11 19:23:51 -0400

    Updates

--- craco.config.js
+++ craco.config.js
@@ -2,9 +2,11 @@ module.exports = {
   style: {
     postcss: {
       plugins: [
+        // eslint-disable-next-line global-require
         require('tailwindcss'),
+        // eslint-disable-next-line global-require
         require('autoprefixer'),
       ],
     },
   },
-}
+};
