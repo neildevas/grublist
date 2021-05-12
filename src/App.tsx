0cf4272cbe92b888147c48a6310e1dbd17f8aeb3
Neil Devas <neil.devas97@gmail.com>
2021-05-11 20:14:45 -0400

    Cleanup + start folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import './App.css'
-import HelloWorld from './HelloWorld'
-import Map from './Map';
+import Index from './components/Map';
 
 function App() {
   return (
-    <div className='App bg-gray-50'>
-      <header className='App-header'>
-        {/*<HelloWorld text='World!' />*/}
-        <Map/>
-      </header>
+    <div className='bg-gray-50'>
+      <Index />
     </div>
   )
 }
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,16 +3,16 @@ import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibmVpbGpvc2VwaCIsImEiOiJja2Vzb2F4cmoxbzFyMnJwbmVyNmRnNDB6In0.jxxscSeTyrVfxCQnWZTYLw';
 
-export default function Map() {
-  const mapContainer = useRef<HTMLInputElement | null>(null);
-  const map = useRef<mapboxgl.Map>(null);
-  const [lng, setLng] = useState(-73.7508592);
-  const [lat, setLat] = useState(40.81518);
-  const [zoom, setZoom] = useState(9);
+export default function Index() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<mapboxgl.Map>();
+  const [lng] = useState(-73.7508592);
+  const [lat] = useState(40.81518);
+  const [zoom] = useState(9);
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return;
     map.current = new mapboxgl.Map({
-      container: mapContainer.current,
+      container: mapContainer.current as HTMLDivElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom
